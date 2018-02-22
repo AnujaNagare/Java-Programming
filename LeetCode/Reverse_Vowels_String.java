cbf70866ba68d2f263e3439c5e3778ddb061a738
Anuja Nagare <anuja.nagare@uga.edu>
2018-02-21 20:29:22 -0500

    Add files via upload

--- LeetCode/Reverse_Vowels_String.java
+++ LeetCode/Reverse_Vowels_String.java
@@ -6,8 +6,8 @@ package GoogleChallenges;
  * Given s = "leetcode", return "leotcede". 
  * soln:
  * - convert string to string array
- * - create one more string array of the same length
- * - go thru string from start to end when u see vowel store it in temp and save its index position and start looking for vowel from the end once u find it replace it with stored vowel
+ * - go thru string from start to end when u see vowel store it in temp and save its index position in start
+ * * start looking for vowel from the end once u find store its index position in end andswap values 
  * 
  */
 public class Reverse_Vowels_String {
@@ -15,16 +15,37 @@ public class Reverse_Vowels_String {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "hello";
-		
+		String vowels = "AEIOUaeiou";
 		char[] cArr= s.toLowerCase().toCharArray();
-		
-		int i=s.length()-1;
-		for (char c: cArr) {
-			if (c == 'a' || c == 'e'||c == 'i'||c == 'o'||c == 'u' || cArr[i] == 'a' || cArr[i] == 'e'|| cArr[i] == 'i'||cArr[i] == 'o'||cArr[i] == 'u') {
-				
+
+
+		if(s != null || s.length()!=0) {
+
+
+			int start = 0;
+			int end = s.length()-1;
+
+			while(start<end){
+
+				while(start<end && !vowels.contains(cArr[start]+"")){
+					start++;
+				}
+
+				while(start<end && !vowels.contains(cArr[end]+"")){
+					end--;
+				}
+
+				char temp = cArr[start];
+				cArr[start] = cArr[end];
+				cArr[end] = temp;
+
+				start++;
+				end--;
 			}
-			i--;
-		}//for
+
+		}//if
+
+		System.out.println(String.valueOf(cArr));
 
 	}
 
